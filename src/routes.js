0f63bfcd436f33e5492d33d0b0bde75a94d4d407
Stuart Dotson <stuart.dotson@opower.com>
2016-06-24 07:36:49 -0400

    set up nested route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 
+const Greeting = () => {
+  return <div>Hey There!</div>;
+};
+
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <Route path="greet" component={Greeting} />
+    <Route path="greet2" component={Greeting} />
+    <Route path="greet3" component={Greeting} />
+  </Route>
 );
